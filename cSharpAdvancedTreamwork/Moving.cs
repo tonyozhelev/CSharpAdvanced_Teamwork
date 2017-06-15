20bdc55ce9d855b91395a7d7de3e32c2710d0733
Ivan Ivanov <i.ivanov.359@gmail.com>
2017-06-15 14:47:23 +0300

    Add files via upload

---
+++ cSharpAdvancedTreamwork/Moving.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invaders
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int redo = 0;
+            int sides = 100;
+            int vertical = 45;
+            var ourShip = "X";
+            ConsoleKeyInfo KeyInfo;
+            Console.CursorVisible = false;
+            Console.SetWindowSize(200, 50);
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Red;
+                
+            do
+            {
+                KeyInfo = Console.ReadKey(true);
+                Console.Clear();
+
+                switch (KeyInfo.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        sides++;
+                        Console.SetCursorPosition(sides, vertical);
+                        Console.WriteLine(ourShip);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        sides--;
+                        Console.SetCursorPosition(sides, vertical);
+                        Console.WriteLine(ourShip);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        vertical--;
+                        Console.SetCursorPosition(sides, vertical);
+                        Console.WriteLine(ourShip);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        vertical++;
+                        Console.SetCursorPosition(sides, vertical);
+                        Console.WriteLine(ourShip);
+                        break;
+                }
+
+            } while (redo == 0);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
