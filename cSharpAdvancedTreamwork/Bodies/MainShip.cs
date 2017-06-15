3636efab60c0e5e6778216513a4abeab47648090
tonyozhelev <tonyo_zhelev@abv.bg>
2017-06-15 15:38:25 +0300

    Added ship movement

---
+++ cSharpAdvancedTreamwork/Bodies/MainShip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpAdvancedTreamwork.Bodies
+{
+    public class MainShip
+    {
+        public MainShip()
+        {
+            PlayerShip = new string[] { "     __     ", "    //\\\\    ", "II  ||||  II","I\\\\\\<<>>///I","II   ^^   II" };
+        }
+        public string[] PlayerShip { get; set; }
+
+        public void DrawShip(int startIndex, int top)
+        {
+            Console.SetCursorPosition(0, top);
+            foreach (var line in PlayerShip)
+            {
+                var lineToDraw = line.PadLeft(startIndex);
+                Console.WriteLine("{0}",lineToDraw);
+            }
+        }
+    }
+}
--- cSharpAdvancedTreamwork/Program.cs
+++ cSharpAdvancedTreamwork/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cSharpAdvancedTreamwork.Bodies;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,58 @@ namespace cSharpAdvancedTreamwork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ship!");
+            var ship = new MainShip();
+            int redo = 0;
+            int sides = 75;
+            int vertical = 45;
+            
+            ConsoleKeyInfo KeyInfo;
+            Console.CursorVisible = false;
+            Console.SetWindowSize(150, 50);
+            ship.DrawShip(sides, vertical);
+            do
+            {
+                KeyInfo = Console.ReadKey(true);
+                Console.Clear();
 
+                switch (KeyInfo.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        if (sides < 149)
+                        {
+                            sides++;
+                        }
+                        ship.DrawShip(sides,vertical);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (sides > 0)
+                        {
+                            sides--;
+                        }
+                        ship.DrawShip(sides, vertical);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (vertical > 0)
+                        {
+                            vertical--;
+                        }
+                        ship.DrawShip(sides, vertical);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (vertical < 50)
+                        {
+                            vertical++;
+                        }
+                        ship.DrawShip(sides, vertical);
+                        break;
+                }
+
+            } while (redo == 0);
+
+            ship.DrawShip(20,40);
+            ship.DrawShip(20,10);
+            
+            Console.ReadKey(true);
         }
     }
 }
